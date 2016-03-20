30f6230f97706637e082d830cf42b2c79c6c7092
Geet Kumar <gkumar7@hawk.iit.edu>
2016-03-20 15:25:59 -0500

    add launch script

---
+++ spark-launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Launch spark cluster
+
+pemFile=$1
+numberOfSlaves=$2
+instanceType=$3
+spotRequest=$4
+
+key=`basename $pemFile .pem`
+
+./spark-ec2 -k $key -i $pemFile -s $numberOfSlaves -r us-east-1 -t $instanceType --hadoop-major-version yarn -v 1.6.0 --spot-price $spotRequest --no-ganglia launch spark-$numberOfSlaves
