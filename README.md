2e626cbe4e25d45b26c8e22c20bb9dbca43b7ecc
Geet Kumar <gkumar7@hawk.iit.edu>
2016-03-25 20:10:43 -0500

    update readme with description of experiments

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ Profiling with spark 1.6.0
 - Spark 1.6.0
 - Hadoop 2.4 (yarn)
 - Amazon Linux AMI (ami-35b1885c)
-- Instance type: m3.medium
+- Instance type: c3.large
 - AWS Region: us-east-1 (Northern Virginia)
+
+#### Experiment Details
+
+##### Terasort
+| Number of Nodes  | Workload (GB) |
+| -------------    | --------------|
+| 1 		   | 100	   |
+| 2  		   | 200  	   |
+| 4 		   | 400 	   |
 
 #### Results
 - results/
--- spark-launch.sh
+++ spark-launch.sh
@@ -9,4 +9,4 @@ spotRequest=$4
 
 key=`basename $pemFile .pem`
 
-./spark-ec2 -k $key -i $pemFile -s $numberOfSlaves -r us-east-1 -t $instanceType --hadoop-major-version yarn -v 1.6.0 --spot-price $spotRequest --no-ganglia launch spark-$numberOfSlaves
+./spark-latest/ec2/spark-ec2 -k $key -i $pemFile -s $numberOfSlaves -r us-east-1 -t $instanceType --hadoop-major-version yarn -v 1.6.0 --spot-price $spotRequest --no-ganglia launch spark-$numberOfSlaves
